fe969c61049de5e21323eb6a9893f520e1d4434a
Miguel Reyes <79011421+3lMibu@users.noreply.github.com>
2021-06-10 00:00:46 -0600

    Add files via upload

---
+++ Runnig Dinosaur/Runnig Dinosaur/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace Runnig_Dinosaur
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.puntaje = new System.Windows.Forms.Label();
+            this.contadorTiempo = new System.Windows.Forms.Timer(this.components);
+            this.tRex = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tRex)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(-7, 397);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(814, 62);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // puntaje
+            // 
+            this.puntaje.AutoSize = true;
+            this.puntaje.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.puntaje.Location = new System.Drawing.Point(13, 13);
+            this.puntaje.Name = "puntaje";
+            this.puntaje.Size = new System.Drawing.Size(165, 33);
+            this.puntaje.TabIndex = 2;
+            this.puntaje.Text = "Puntaje: 0";
+            // 
+            // contadorTiempo
+            // 
+            this.contadorTiempo.Interval = 20;
+            this.contadorTiempo.Tick += new System.EventHandler(this.contadorJuegoPrincipal);
+            // 
+            // tRex
+            // 
+            this.tRex.Image = global::Runnig_Dinosaur.Properties.Resources.running1;
+            this.tRex.Location = new System.Drawing.Point(53, 354);
+            this.tRex.Name = "tRex";
+            this.tRex.Size = new System.Drawing.Size(40, 43);
+            this.tRex.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.tRex.TabIndex = 3;
+            this.tRex.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Runnig_Dinosaur.Properties.Resources.obstacle_1;
+            this.pictureBox3.Location = new System.Drawing.Point(311, 351);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(23, 46);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "obstaculo";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Runnig_Dinosaur.Properties.Resources.obstacle_2;
+            this.pictureBox4.Location = new System.Drawing.Point(509, 364);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(32, 33);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "obstaculo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.tRex);
+            this.Controls.Add(this.puntaje);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Running Dinosaur";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tRex)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label puntaje;
+        private System.Windows.Forms.Timer contadorTiempo;
+        private System.Windows.Forms.PictureBox tRex;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ Runnig Dinosaur/Runnig Dinosaur/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Runnig_Dinosaur
+{
+    //Miguel Daniel Reyes Martinez
+    //Maynor Antonio Torres Hernandez
+
+    
+    public partial class Form1 : Form //variables a usarse
+    {
+        bool salto = false; //salto
+        int velocidadSalto; //la velocidad del salto
+        int fuerza = 12; //la fuerza del salto
+        int score = 0;  //el puntaje
+        int velocidadObstaculo = 10; //velocidad de movimiento de los obstaculos
+        Random rand = new Random(); //un metodo random que servira para posicionar los obstaculos
+        int posicion; //la posicion del dinosaurio en pantalla
+        bool gameOver = false; //un game over
+        SoundPlayer jump = new SoundPlayer("jump.wav");
+        SoundPlayer losing = new SoundPlayer("losing.wav"); //todas estan guardadas en debug/netcoreApp3.1
+        SoundPlayer rdy = new SoundPlayer("ready.wav");
+        SoundPlayer point = new SoundPlayer("point.wav");
+        SoundPlayer serious = new SoundPlayer("serious.wav");
+        //SoundPlayer bckground = new SoundPlayer("corona.wav");
+        public Form1()
+        {
+            InitializeComponent(); //jamas nunca borrar!
+
+            resetJuego(); //se declara la clase aqui para que se inicie con los valores iniciales 
+            rdy.Play();
+            //bckground.PlayLooping();
+           
+        }
+
+        private void contadorJuegoPrincipal(object sender, EventArgs e)  //contador principal, donde esta la mayoria de la logica
+        {
+            tRex.Top += velocidadSalto;     
+            puntaje.Text = "Puntaje: " + score; //el indicador del puntaje obtenido
+
+            if (salto == true && fuerza < 0) 
+            {
+                salto = false;
+            }
+            if (salto == true)
+            {
+                velocidadSalto = -12;
+                fuerza -= 1;
+            }
+            else
+            {
+                velocidadSalto = 12;
+            }
+
+            if (tRex.Top > 353 && salto == false)
+            {
+                fuerza = 12;
+                tRex.Top = 354;
+                velocidadSalto = 0;
+            }
+
+            foreach(Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "obstaculo") //se controla la velocidad de los obstaculos, 
+                {
+                    x.Left -= velocidadObstaculo;
+
+                    if (x.Left < -100)
+                    {
+                        x.Left = this.ClientSize.Width + rand.Next(200, 500) + (x.Width * 15); //en pixeles va ubicando los obstaculos
+                        score++;
+                        point.Play();
+
+                    }
+
+                    if (tRex.Bounds.IntersectsWith(x.Bounds)) //la colision o la hitbox, si toca el obstaculo se muere y gameover
+                    {
+                        contadorTiempo.Stop();   //se para el timer
+                        tRex.Image = Properties.Resources.dead;  //cambia a la imagen de muerto
+                        puntaje.Text = " Presiona R para volver a comenzar! "; //texto de aviso
+                        gameOver = true; //termina el juego
+                        losing.Play();
+                    }
+                }
+                              
+            }
+
+            if (score >= 10) //al llegar a 10 el puntaje se aumenta la velocidad de los obstaculos
+            {
+                
+                velocidadObstaculo = 15;
+
+            }
+
+            if (score >= 20)
+            {
+                velocidadObstaculo = 20;
+            }
+
+            if (score >= 30)
+            {
+                velocidadObstaculo = 25;
+            }
+
+        }
+
+        private void keyIsDown(object sender, KeyEventArgs e) //evento si la key esta abajo
+        {
+            if (e.KeyCode == Keys.Space && salto == false)  //cambiamos abajo por espacio
+            {
+                salto = true;   //salta el dino
+                jump.Play();
+            }
+        }
+
+        private void keyIsUp(object sender, KeyEventArgs e) //evnto si la key esta arriba
+        {
+          if (salto==true)
+            {
+                salto = false;
+            }
+
+          if (e.KeyCode==Keys.R && gameOver==true)  //si se pierde el juego se reinicia con la letra R
+            {
+                resetJuego(); //hace un llamado a la clase de reset
+            }
+        }
+
+        private void resetJuego() //valores iniciales con los que se comenzara el juego
+        {
+            rdy.Play();
+            fuerza = 12;
+            velocidadSalto = 0;
+            salto = false;
+            score = 0;
+            velocidadObstaculo = 10;
+            puntaje.Text = "Puntaje: " + score;          //el puntaje 
+            tRex.Image = Properties.Resources.running; //la imagen del dino corriendo
+            gameOver = false;
+            tRex.Top = 354;  //posicion en donde comenzara el dinosaurio
+            
+
+            foreach (Control x in this.Controls) //un foreach para controlar los obstaculos al mismo tiempo
+            {
+                if (x is PictureBox && (string)x.Tag == "obstaculo") //se tagean ambas picturebox con el nombre de tag, para que sea mas facil
+                {
+                    posicion = this.ClientSize.Width + rand.Next(500, 800) + (x.Width * 10); //de forma random se asigna la posicion de los obstaculos al comienzo de cada partida
+                    x.Left = posicion;
+                }
+            }
+
+            contadorTiempo.Start(); //se inicia el timer
+
+        }
+    }
+}
